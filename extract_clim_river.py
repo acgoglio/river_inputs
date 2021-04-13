0a64605000010035637a62f267c2406d861c0655
Anna Chiara Goglio <ag15419@n295.cluster.net>
2021-04-13 14:46:49 +0200

    Ready extract_clim_river.py code

--- extract_clim_river.py
+++ extract_clim_river.py
@@ -22,119 +22,53 @@ from scipy import interpolate
 # by AC Goglio (CMCC)
 # annachiara.goglio@cmcc.it
 #
-# Written: 32/04/2021
+# Written: 13/04/2021
 #
-# This script reads the output (txt format) x_killworth_time_interpolation.exe
-# does the daily interpolation from monthly pseudodischarge 
-# and builds the NETCDF daily river input file for EAS System
+# This script reads the EAS system monthly river file
+# and extracts the climatological values to txt files
 #
 #################################################################
-#climatological_values=np.array([426.3500,370.3400,383.3800,460.9100,481.9800,589.7000,552.0400,358.0800,280.4800,394.3600,530.1700,557.8900,426.3500,370.3400])
-climatological_values=np.array([370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,370.3400,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,383.3800,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,460.9100,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,481.9800,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,589.7000,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,552.0400,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,358.0800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,280.4800,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,394.3600,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,530.1700,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,557.8900,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500,426.3500])
-pseudodischarge=np.array([451.2103,356.2527,373.1344,476.3719,461.4091,613.7565,576.7563,338.7299,251.6019,393.1775,544.3704,583.7927,412.5397,348.9618])
-timeintoout=np.array([15,45,76,106,135,166,196,227,257,288,319,349,380,410])
-f = interpolate.interp1d(timeintoout,pseudodischarge)
-xnew = np.arange(32, 396, 1.0)
-x_clim=np.arange(1, 366, 1.0)
-ynew = f(xnew)
-offset=31
+# Read line arg (name of input file)
 
-print ('xnew:',xnew )
-print ('ynew:',ynew )
+monthly_rivers=sys.argv[1]
+print ('Input file is: ',monthly_rivers)
 
-# Plots Per grid and per part
-plotname='comp_py.png'
-plt.figure(figsize=(18,12))
-plt.rc('font', size=16)
-plt.title ('From climatological discharge to daily discharge --- Po dritta')
-plt.plot(x_clim,climatological_values,label = 'Climatological values')
-plt.plot(timeintoout-offset,pseudodischarge,'o',label = 'Pseudodischarge (Killworth)')
-plt.plot(xnew-offset,ynew,label = 'Daily values')
+# Open the file and read the vars
+input_monthly = NC.Dataset(monthly_rivers,'r')
+# Read the grid
+lat_2d='nav_lat'
+lon_2d='nav_lon'
+lons = input_monthly.variables[lon_2d][:]
+lats = input_monthly.variables[lat_2d][:]
+lat_idx='y'
+lon_idx='x'
+# Read the mask and the runoff values 
+mask_var='river_msk'
+mask = input_monthly.variables[mask_var][:]
 #
-plt.grid ()
-plt.xlim(1,365)
-#plt.axhline(y=0 color='black')
-plt.ylabel ('River discharge [kg/m2/s]')
-plt.xlabel ('Days of the year')
-plt.legend() #bbox_to_anchor=(1.04,1), loc="upper left")
-# Save and close 
-plt.savefig(plotname)
-plt.clf()
+runoff_var='sorunoff'
+runoff = input_monthly.variables[runoff_var][:]
+# Loop on lat and lon to find and extract river climatological monthly values (1 ts for each grid point)
+print ('len(lons): ',len(lons[1]),' len(lats): ',len(lats[:,0]))
+river_num=0
+for idx_lon in range (0,len(lons[0])):
+    for idx_lat in range (0,len(lats)):
+        print ('Indexes: ', idx_lon, idx_lat )
 
-#fields=['v','u','z']
-#grids=['V','U','T']
-#udm=['m/s','m/s','m']
-#tidal_comp=['M2','S2','N2','K2','K1','O1','P1','Q1']
-#
-## Loop on grids <-> input tabs
-#for idx_grid,grid in enumerate(grids):
-#    print ('I am working on grid: ',grid)
-#    # Selection of the field 
-#    field=fields[idx_grid]
-#    # Units
-#    units=udm[idx_grid]
-#    print ('I am writing Re and Im part of field: ',field,' [',units,']')
-#
-#    # Build the tab name
-#    intab_name='bdytide_tab_grid'+grid+'.txt'
-#    print ('Reading filed values from: ',intab_name )
-#
-#    # Read the values from the input tab
-#    intab=pd.read_csv(intab_name,sep=';',usecols=['LAT','LON','M2_RE','M2_IM','S2_RE','S2_IM','N2_RE','N2_IM','K2_RE','K2_IM','K1_RE','K1_IM','O1_RE','O1_IM','P1_RE','P1_IM','Q1_RE','Q1_IM'])
-#
-#    # Loop on tidal components <-> output files
-#    for t_comp in tidal_comp:
-#       print ('I am working on tidal componenent: ',t_comp)
-#
-#       # Build the nc name
-#       outnc_name='mfs_bdytide_'+t_comp+'_grid_'+grid+'.nc'
-#       print ('I am writing reslts to outfile: ',outnc_name)
-#
-#       # Open the nc
-#       outnc = NC.Dataset(outnc_name,'a')
-#
-#       # Read the coordinates  
-#       b_lat='nav_lat'
-#       b_lon='nav_lon'
-#       lat_idx='yb'
-#       lon_idx='xb'+grid
-#       lons = outnc.variables[b_lon][:,:]
-#       lats = outnc.variables[b_lat][:,:]
-#       print ('The dimension names in the out file are: ',lat_idx,lon_idx)
-#       print ('The coordinate names in the out file are: ',b_lat,b_lon)
-#
-#       # Create the new fields
-#       print ('New fileds storing Re and Im part are: ',field,'1',' and ',field,'2')
-#       # RE
-#       globals()[field+'1']=outnc.createVariable(field+'1',np.float64,(lat_idx,lon_idx))
-#       globals()[field+'1'].units = units
-#       globals()[field+'1'].standard_name = t_comp+' '+field+' Real part'
-#       # IM
-#       globals()[field+'2']=outnc.createVariable(field+'2',np.float64,(lat_idx,lon_idx))
-#       globals()[field+'2'].units = units
-#       globals()[field+'2'].standard_name = t_comp+' '+field+' Immaginary part'
-#   
-#       # Loop on bdy points:
-#       print ('I am going to write the field along the bdy: len(lons): ',len(lons[0,:]),' len(lats): ',len(lats))
-#       idx_bdy_coo=0
-#   
-#       try:
-#          for idx_lon in range (0,len(lons[0,:])):
-#              for idx_lat in range (0,len(lats)):
-#                  print ('Indexes: ', idx_lon, idx_lat )
-#    
-#                  # Write values in the arrays
-#                  globals()[field+'1'][idx_lat,idx_lon]=intab[t_comp+'_'+'RE'][idx_bdy_coo]
-#                  globals()[field+'2'][idx_lat,idx_lon]=intab[t_comp+'_'+'IM'][idx_bdy_coo]
-#   
-#                  idx_bdy_coo=idx_bdy_coo+1
-#   
-#   
-#          outnc.close()
-#
-#       except:
-#          print ('ERROR..')
-#          outnc.close()
-
-
+        # Read the river_mask value
+        mask_val = mask[0,idx_lat,idx_lon]
+        if mask_val == 1:
+           river_num=river_num=+1
+           print ('Found river ',river_num) 
+           # build the climatological file name
+           clim_filename='clim_'+str(idx_lon)+'_'+str(idx_lat)+'.txt'
+           # Read 12 monthly clim values 
+           runoff_clim=runoff[:,idx_lat,idx_lon]
+           #runoff_clim_new=runoff_clim*10000.0
+           runoff_clim.tolist()
+           # Write values to clim file
+           clim_file = open(clim_filename,"w")
+           print(*runoff_clim,sep = " ",file=clim_file)
+           clim_file.close()
+      
 #################################################################

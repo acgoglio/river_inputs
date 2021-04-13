1f5861d472f1eb0950525d126e98e0ba9e7b2dc8
Anna Chiara Goglio <ag15419@n264.cluster.net>
2021-04-13 15:44:51 +0200

    Last version

---
+++ killworth_temp.F90
@@ -0,0 +1,260 @@
+      !=====================================================================!
+      !         This program performs time interpolation on the basis       !
+      !         of Killworth, 1996                                          !
+      !                                                                     !
+      !  D. Delrosso, 24th November 2016                                    !
+      !=====================================================================!
+
+
+      PROGRAM killworth_time_interpolation
+      USE netcdf
+      IMPLICIT NONE
+
+!     DECLARATION SECTION 
+      CHARACTER (len = 100)     :: argclim          ! climatology .txt to read (argument)     
+      REAL, DIMENSION(1,12)     :: climrvr          ! climatological values series
+      REAL, DIMENSION(1,14)     :: extclimrvr       ! climatological values extended series     
+      INTEGER                   :: k                ! loop indexes
+      INTEGER                   :: i, j, z          ! loop indexes
+      INTEGER, DIMENSION(1,16)  :: l                ! number of days in each month 
+      REAL, DIMENSION(1,14)     :: e, g, f          ! matrix coefficients
+      REAL, DIMENSION(14,14)    :: A                ! matrix
+      REAL, DIMENSION(14,14)    :: AA               ! inverse matrix of A
+      REAL, DIMENSION(14,14)    :: UNI
+      REAL, DIMENSION(1,14)     :: pseudodischarge  ! pseudodischarge matrix      
+      REAL                      :: l1, lm1, lp1
+      INTEGER                   :: nx, ny           ! dimensions of matrix A
+!     FINDInv subroutine section
+      REAL, ALLOCATABLE, DIMENSION(:,:) :: Matrix, invMatrix
+      INTEGER                           :: n, ErrorFlag
+!     interp1 subroutine section
+      REAL, ALLOCATABLE, DIMENSION(:,:) :: xData, yData,xd,yd
+      REAL, ALLOCATABLE, DIMENSION(:,:) :: xVal
+      INTEGER                           :: ikm, okm,nd,ni
+      REAL, ALLOCATABLE, DIMENSION(:,:) :: yVal,yi
+      INTEGER                           :: inputIndex, dataIndex
+      REAL, ALLOCATABLE, DIMENSION(:,:) :: weight
+      REAL                              :: minXdata, minYdata, xRange
+!     linear interpolation section
+      INTEGER, DIMENSION(1,14)              :: timein,timeintotimeout          ! Array from [1:14] number
+      INTEGER                               :: it, ot          ! Loop indexes
+      INTEGER                               :: countertime,counterdays     ! Time counter
+
+!     EXECUTION SECTION
+
+!     Read input file
+      CALL getarg(1, argclim)
+
+      OPEN(10,file=argclim,status='old')
+      READ(10,*) climrvr
+      CLOSE(10)
+
+
+!     Extend climatological series for ensure cyclicity
+      DO k=1,12
+        extclimrvr(1,k+1) = climrvr(1,k)
+      END DO
+
+      extclimrvr(1,1)     = climrvr(1,12)
+      extclimrvr(1,14)    = climrvr(1,1)        
+
+
+!     General formulation for getting daily values from monthly values
+!     b = A * d  where b are daily values, A is a tridiagonal matrix 
+!     and d are climatological values      
+      j = 1
+      l = RESHAPE((/ 30,31,31,28,31,30,31,30,31,31,30,31,30,31,31,28 /), SHAPE(l)) 
+      ! november to february number of days
+
+      DO i = 2,15
+        l1 = l(1,i)
+        lm1= l(1,i-1)
+        lp1= l(1,i+1)        
+        e(1,j)= l1 / ( 4 * (lm1 + l1) )    
+        g(1,j)= l1 / ( 4 * (l1  + lp1))
+        f(1,j)= 1  - e(1,j) - g(1,j)
+        j=j+1        
+
+      END DO     
+
+
+!     Matrix A definition
+      A(1,:)  = (/ f(1,1),g(1,1),0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,e(1,1) /)
+      A(2,:)  = (/ e(1,2),f(1,2),g(1,2),0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 /)
+      A(3,:)  = (/ 0.0,e(1,3),f(1,3),g(1,3),0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 /)
+      A(4,:) = (/ 0.0,0.0,e(1,4),f(1,4),g(1,4),0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 /)
+      A(5,:) = (/ 0.0,0.0,0.0,e(1,5),f(1,5),g(1,5),0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 /)
+      A(6,:) = (/ 0.0,0.0,0.0,0.0,e(1,6),f(1,6),g(1,6),0.0,0.0,0.0,0.0,0.0,0.0,0.0 /)
+      A(7,:) = (/ 0.0,0.0,0.0,0.0,0.0,e(1,7),f(1,7),g(1,7),0.0,0.0,0.0,0.0,0.0,0.0 /)
+      A(8,:) = (/ 0.0,0.0,0.0,0.0,0.0,0.0,e(1,8),f(1,8),g(1,8),0.0,0.0,0.0,0.0,0.0 /)
+      A(9,:) = (/ 0.0,0.0,0.0,0.0,0.0,0.0,0.0,e(1,9),f(1,9),g(1,9),0.0,0.0,0.0,0.0 /)
+      A(10,:) = (/ 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,e(1,10),f(1,10),g(1,10),0.0,0.0,0.0 /)
+      A(11,:) = (/ 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,e(1,11),f(1,11),g(1,11),0.0,0.0 /)
+      A(12,:) = (/ 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,e(1,12),f(1,12),g(1,12),0.0 /)
+      A(13,:) = (/ 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,e(1,13),f(1,13),g(1,13) /)
+      A(14,:) = (/ g(1,14),0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,e(1,14),f(1,14) /)
+
+      
+      nx = SIZE(A(1,:))
+      ny = SIZE(A(:,1))
+
+!     Compute AA inverse matrix of A
+      CALL FindInv(A,AA,nx,ErrorFlag)
+
+!     Compute pseudodischarge d' = AA * d
+      CALL MULT(pseudodischarge,extclimrvr,AA,nx,nx,nx)
+
+      ! Create arrays for time interpolation
+      countertime = 1
+      counterdays = 0
+      DO it = 1,14
+        timein(1,it) = countertime
+        countertime  = countertime +1
+        IF ( it == 1) THEN
+           timeintotimeout(1,it)=FLOOR(l(1,it)/2.0)
+        ELSE
+           timeintotimeout(1,it)=counterdays+FLOOR(l(1,it)/2.0)
+        END IF
+        counterdays = counterdays + l(1,it)
+      END DO 
+ 
+      ! Print pseudoscharge in the middle of the current month
+      DO it = 1,14
+         PRINT*,timeintotimeout(1,it),pseudodischarge(1,it)
+      END DO
+
+
+      END PROGRAM killworth_time_interpolation
+
+
+        !Subroutine to find the inverse of a square matrix
+        !Author : Louisda16th a.k.a Ashwith J. Rego
+        !Reference : Algorithm has been well explained in:
+        !http://math.uww.edu/~mcfarlat/inverse.htm           
+        !http://www.tutor.ms.unimelb.edu.au/matrix/matrix_inverse.html
+
+        SUBROUTINE FINDInv(matrix, inverse, n, errorflag)
+        IMPLICIT NONE
+        !Declarations
+        INTEGER, INTENT(IN) :: n
+        INTEGER, INTENT(OUT) :: errorflag  !Return error status. -1 for error, 0 for normal
+        REAL, INTENT(IN), DIMENSION(n,n) :: matrix  !Input matrix
+        REAL, INTENT(OUT), DIMENSION(n,n) :: inverse !Inverted matrix
+        
+        LOGICAL :: FLAG = .TRUE.
+        INTEGER :: i, j, k, l
+        REAL :: m
+        REAL, DIMENSION(n,2*n) :: augmatrix !augmented matrix
+        
+        !Augment input matrix with an identity matrix
+        DO i = 1, n
+                DO j = 1, 2*n
+                        IF (j <= n ) THEN
+                                augmatrix(i,j) = matrix(i,j)
+                        ELSE IF ((i+n) == j) THEN
+                                augmatrix(i,j) = 1
+                        Else
+                                augmatrix(i,j) = 0
+                        ENDIF
+                END DO
+        END DO
+        
+        !Reduce augmented matrix to upper traingular form
+        DO k =1, n-1
+                IF (augmatrix(k,k) == 0) THEN
+                        FLAG = .FALSE.
+                        DO i = k+1, n
+                                IF (augmatrix(i,k) /= 0) THEN
+                                        DO j = 1,2*n
+                                                augmatrix(k,j) = augmatrix(k,j)+augmatrix(i,j)
+                                        END DO
+                                        FLAG = .TRUE.
+                                        EXIT
+                                ENDIF
+                                IF (FLAG .EQV. .FALSE.) THEN
+                                        PRINT*, "Matrix non-invertible"
+                                        inverse = 0
+                                        errorflag = -1
+                                        return
+                                ENDIF
+                        END DO
+                ENDIF
+                DO j = k+1, n                   
+                        m = augmatrix(j,k)/augmatrix(k,k)
+                        DO i = k, 2*n
+                                augmatrix(j,i) = augmatrix(j,i) - m*augmatrix(k,i)
+                        END DO
+                END DO
+        END DO
+        
+        !Test for invertibility
+        DO i = 1, n
+                IF (augmatrix(i,i) == 0) THEN
+                        PRINT*, "Matrix is non - invertible"
+                        inverse = 0
+                        errorflag = -1
+                        return
+                ENDIF
+        END DO
+        
+        !Make diagonal elements as 1
+        DO i = 1 , n
+                m = augmatrix(i,i)
+                DO j = i , (2 * n)                              
+                           augmatrix(i,j) = (augmatrix(i,j) / m)
+                END DO
+        END DO
+        
+        !Reduced right side half of augmented matrix to identity matrix
+        DO k = n-1, 1, -1
+                DO i =1, k
+                m = augmatrix(i,k+1)
+                        DO j = k, (2*n)
+                                augmatrix(i,j) = augmatrix(i,j) -augmatrix(k+1,j) * m
+                        END DO
+                END DO
+        END DO                          
+        
+        !store answer
+        DO i =1, n
+                DO j = 1, n
+                        inverse(i,j) = augmatrix(i,j+n)
+                END DO
+        END DO
+        errorflag = 0
+        END SUBROUTINE FINDinv
+
+        SUBROUTINE MULT(Y,X,A,LA,M,N)
+!     |________________________________________________________|
+!     |       MULTIPLY A GENERAL REAL MATRIX BY A VECTOR       |
+!     |                                                        |
+!     |    INPUT:                                              |
+!     |                                                        |
+!     |         X     --VECTOR                                 |
+!     |                                                        |
+!     |         A     --ARRAY CONTAINING COEFFICIENT MATRIX    |
+!     |                                                        |
+!     |         LA    --LEADING (ROW) DIMENSION OF ARRAY A     |
+!     |                                                        |
+!     |         M     --NUMBER OF ROWS IN COEFFICIENT MATRIX   |
+!     |                                                        |
+!     |         N     --NUMBER OF COLUMNS IN COEFF. MATRIX     |
+!     |                                                        |
+!     |    OUTPUT:                                             |
+!     |                                                        |
+!     |         Y     --PRODUCT BETWEEN A AND X                |
+!     |________________________________________________________|
+!
+      INTEGER I,J,M,N,LA
+      REAL A(LA,1),X(1),Y(1),T
+      DO 10 I = 1,M
+10         Y(I) = 0.
+      DO 20 J = 1,N
+           T = X(J)
+           DO 30 I = 1,M
+              Y(I) = Y(I) + A(I,J)*T
+ 30        continue
+ 20   continue
+      RETURN
+      END SUBROUTINE MULT
+
--- run_dailyriver.sh
+++ run_dailyriver.sh
@@ -9,9 +9,12 @@
 set -e
 #set -x 
 ########################
-echo "*********** Daily river input 4 EAS System *********"
+echo "*********** Daily river input 4 EAS System ***********"
 
 ####################### SET THE FOLLOWING VARS: ############################################
+# Year
+YEAR2COMPUTE=2015
+
 # Workdir and your virtual environment
 WORKDIR="/users_home/oda/ag15419/river_inputs/Killworth/"
 YOUR_PY_ENV="mappyenv"
@@ -52,12 +55,38 @@ fi
 module load anaconda 
 source activate $YOUR_PY_ENV
 
-python $EXE_CLIM $MONTHLY_RIVERS
+# Clean the directory from previous outputs
+echo "Clean the workdir from previous run outputs.."
+sleep 3
+for TOBERM in $( ls $WORKDIR/clim_*.txt); do
+    rm -vr $TOBERM
+done
 
+echo "I am going to extract climatological values from input file: $MONTHLY_RIVERS ..."
+python $EXE_CLIM $MONTHLY_RIVERS
+echo "...Done!!"
 
 
 # Load the environment for step 2
 module load intel19.5/19.5.281 impi19.5/19.5.281 impi19.5/netcdf/C_4.7.2-F_4.5.2_CXX_4.3.1
+
+# Loop on days and months (from november of year-1 to february year+1 )
+# Built start date
+START_YEAR=$(( $YEAR2COMPUTE - 1 ))
+NEXT_START_MONTH=12
+START_DATE=${START_YEAR}${NEXT_START_MONTH}01
+# Loop on months
+IDX_DATE=$START_DATE
+IDX_MONTH=0
+while [[ $IDX_MONTH -lt 16 ]]; do   
+      echo $( date -u -d "${IDX_DATE} -1 day" +%Y%m%d )
+      IDX_DATE=$( date -u -d "${IDX_DATE} 1 month" +%Y%m%d )
+      IDX_MONTH=$(( $IDX_MONTH + 1 ))
+done
+
+  # Built and Compile the executable
+
+  # Run the executable
 
 
 # Load the environment for step 3
@@ -65,95 +94,3 @@ module load $YOUR_PY_ENV
 source activate mappyenv
 
 ######################
-
-
-# Tidal components
-TIDAL_U_COMPONENTS=('M2' 'S2' 'N2' 'K2' 'K1' 'O1' 'P1' 'Q1')
-# WARNING: the order of tidal components MUST be the same given in the f90 code..
-#          Remember to check it also in the py script!  
-
-# Bdy infiles template (NEMO bdy files) 
-INBDY_TEMPLATE='mfs_bdy%grid%_u2d_*.nc'
-OUTBDY_TEMPLATE_TAB_TEMP='bdytide_tab_grid%grid%.txt' 
-OUTBDY_TEMPLATE_PRE='mfs_bdytide'
-echo "Input bdy file templat: ${INBDY_TEMPLATE}"
-echo "Intermediate txt table: ${OUTBDY_TEMPLATE_TAB}"
-
-# Outfields to grids connection
-OUTFIELD=("z" "u" "v")
-INGRIDS=("T" "U" "V") 
-UDM=("cm" "cm/s" "cm/s")
-
-# Loop on out fields/grids (z/T; u/U; v/V )
-for ID_FIELDGRID in 0 1 2; do
-
-   echo "I am building tidal bdy field (Re and Imm): ${OUTFIELD[${ID_FIELDGRID}]} [${UDM[${ID_FIELDGRID}]}] on bdy grid: ${INGRIDS[$ID_FIELDGRID]}"
-
-   # Check bdy input file existence
-   INBDY=$( ls $( echo $INBDY_TEMPLATE | sed -e "s/%grid%/${INGRIDS[${ID_FIELDGRID}]}/g") )
-   if [[ -e $INBDY ]]; then
-      echo "I am reading coordinates from ${INBDY}!"
-
-   else
-      echo "ERROR: Input file ${INBDY} NOT found!"
-   fi
-
-   # Build the outfiles
-   echo "I am building the outfile templates.."
-   for ID_TCOMP in ${TIDAL_U_COMPONENTS[@]}; do
-       OUTBDY=${OUTBDY_TEMPLATE_PRE}_${ID_TCOMP}_grid_${INGRIDS[$ID_FIELDGRID]}.nc
-       cp $INBDY ${OUTBDY}
-       echo "${OUTBDY} ready"
-   done
-
-   # Set the arg
-   ARG_ZUV=${OUTFIELD[${ID_FIELDGRID}]}
-   ARG_INFILE=${INBDY}
-   ARG_OUTFILE_TAB=$( echo $OUTBDY_TEMPLATE_TAB_TEMP | sed -e "s/%grid%/${INGRIDS[${ID_FIELDGRID}]}/g") 
-
-   echo "I am going to run the extractor with the following args: $ARG_ZUV $ARG_INFILE tmp_$ARG_OUTFILE_TAB"
-
-   # Run the extraction script
-   echo "I am going to execute the following command: ./${EXE_NAME} $ARG_ZUV $ARG_INFILE tmp_$ARG_OUTFILE_TAB"
-   ./${EXE_NAME} $ARG_ZUV $ARG_INFILE tmp_$ARG_OUTFILE_TAB
-
-   # Define and write the new tab header
-   LINE_STR_1=$( head -n 3 tmp_$ARG_OUTFILE_TAB | tail -n 1 )
-   LINE_STR_2=$( head -n 4 tmp_$ARG_OUTFILE_TAB | tail -n 1 )
-   NEW_HEADER=$( echo "${LINE_STR_1^^}${LINE_STR_2^^}" | sed -e "s/ /;/g" | sed -e "s/;;;/;/g" | sed -e "s/;;/;/g" | sed -e "s/;;/;/g" | sed -e "s/;_/_/g" ) 
-   echo $NEW_HEADER > $ARG_OUTFILE_TAB
-
-   # Cut the old header from the out tab
-   tail -n +5 tmp_$ARG_OUTFILE_TAB | sed -e "s/ /;/g" | sed -e "s/;;;/;/g" | sed -e "s/;;/;/g" | sed -e "s/;;/;/g" >> $ARG_OUTFILE_TAB
-   echo "....Done!!"
-
-   # Read the field into the tab and write it into the nc file
-   
-done 
-
-# Clean the directory
-rm tmp_*.txt
-
-####################### TIDAL BDY FIELDS BUILDING  ############################################
-
-echo "Tidal bdy files building.."
-
-# Load the environment
-#module purge
-#module load anaconda
-#source activate mappyenv
-
-B_EXE_NAME='tbdy_writer.py'
-python ${B_EXE_NAME}
-
-# Set title and author in the outfiles and remove extra fields
-module purge
-module load anaconda/3.7 curl/7.70.0 cmake/3.17.3 gams/28.2.0 gcc_9.1.0/9.1.0 gcc_9.1.0/gempack/12.885 gcc_9.1.0/OpenBLAS/0.3.9 gcc_9.1.0/papi/6.0.0 gcc_9.1.0/R/3.6.1 modules mysql/5.7.28 ncl/6.6.2 sqlite/3.32.2 subversion/1.14.0 wgrib/1.8.1.0b impi20.1/19.7.217 impi20.1/esmf/8.0.1-intelmpi-64-g impi20.1/hdf5/1.12.0 impi20.1/hdf5-threadsafe/1.12.0 impi20.1/netcdf/C_4.7.4-F_4.5.3_CXX_4.3.1 impi20.1/netcdf-threadsafe/C_4.7.4-F_4.5.3_CXX_4.3.1 impi20.1/papi/6.0.0 impi20.1/parallel-netcdf/1.12.1 impi20.1/petsc/3.13.2 impi20.1/zoltan/3.8 intel20.1/20.1.217 intel20.1/advisor intel20.1/boost/1.73.0 intel20.1/cdo/1.9.8 intel20.1/cnvgrib/3.1.1 intel20.1/eccodes/2.17.0 intel20.1/esmf/8.0.1-mpiuni-64-g intel20.1/esmf/8.0.1-mpiuni-64-O intel20.1/exactextract/545f0d6 intel20.1/g2lib/3.1.0 intel20.1/gdal/3.1.0 intel20.1/hdf5/1.12.0 intel20.1/hdf5-threadsafe/1.12.0 intel20.1/inspector intel20.1/itac intel20.1/libemos/4.5.9 intel20.1/libemos/4.5.9 intel20.1/magics/3.3.1 intel20.1/nco/4.9.3 intel20.1/ncview/2.1.8 intel20.1/netcdf/C_4.7.4-F_4.5.3_CXX_4.3.1 intel20.1/netcdf-threadsafe/C_4.7.4-F_4.5.3_CXX_4.3.1 intel20.1/proj/7.0.1 intel20.1/R/4.0.2 intel20.1/szip/2.1.1 intel20.1/udunits/2.2.26 intel20.1/valgrind/3.16.0 intel20.1/vtune intel20.1/w3lib/2.0.6 intel20.1/wgrib2/2.0.8
-
-for TOBEMOD in $( ls ${OUTBDY_TEMPLATE_PRE}*.nc ); do
-    mv $TOBEMOD ${TOBEMOD}_tmp
-    ncatted -O -h -a title,global,m,c,"tidal bdy data from TPXO model" ${TOBEMOD}_tmp
-    ncatted -O -h -a author,global,m,c,"Anna Chiara Goglio - CMCC" ${TOBEMOD}_tmp
-    cdo delete,name=sossheig,vobtcrtx,vobtcrty ${TOBEMOD}_tmp $TOBEMOD
-    rm ${TOBEMOD}_tmp
-done
